7c62b6c4c2f263070de1ef96374cf3910fb37119
rohitmsaradhi <30939463+rohitmsaradhi@users.noreply.github.com>
2017-08-18 04:11:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,11 @@
 counter code
-var button=documnet.getelements
+var button=documnet.getelementsbyid('counter'):
+button.onclick=funtion(){
+    // make a request to the counter endpoint
+    // capture the response and store it in a variable
+    
+    // render th variable in the correct span
+    counter= counter+1
+    var span=document.getelementbyid('count')
+    span.inner html=counter to spring
+}
